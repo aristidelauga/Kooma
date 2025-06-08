616602487a7bd79ec085f562b30f643b2bc111fa
aristidelauga <aristide.lauga@gmail.com>
2025-06-08 14:12:52 +0200

    Initial Commit

---
+++ Kooma/KoomaApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoomaApp.swift
+//  Kooma
+//
+//  Created by Aristide LAUGA on 08/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct KoomaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
