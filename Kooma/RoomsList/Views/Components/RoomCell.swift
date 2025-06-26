e887cb1f53de2963d0dc92d11d66dd6dbc134377
aristidelauga <aristide.lauga@gmail.com>
2025-06-26 21:26:15 +0200

    RoomCell improved

--- Kooma/RoomsList/Views/Components/RoomCell.swift
+++ Kooma/RoomsList/Views/Components/RoomCell.swift
@@ -34,37 +34,61 @@ struct RoomCell: View {
 			Image(room.image)
 				.resizable()
 				.frame(maxWidth: .infinity, maxHeight: 140)
+                .cornerRadius(12, corners: [.topLeft, .topRight])
+            
 
 			VStack(alignment: .leading, spacing: 6){
 				TextHeading600(text: room.name ?? "Work Lunch Room")
-
-				HStack(spacing: 0) {
-					TextBodyMedium(text: self.room.administrator.name)
-						.bold()
-					TextBodyMedium(text: "is the administrator")
-						.padding(.leading, 5)
-				}
-
+                
+                Text(.init("**\(self.room.administrator.name)** is the administrator"))
+                    .font(.bodyLarge)
+                    .foregroundStyle(.kmBlack)
+                    .fixedSize(horizontal: false, vertical: true)
+                
 				Text("^[\(String(describing: self.room.members.count)) member](inflect: true)")
-					.font(.heading200)
+                    .font(.bodyMedium)
 					.foregroundStyle(.kmKaki)
-
+                    .padding(.bottom, 12)
 			}
+            .padding(.horizontal, 8)
 		}
-		.frame(maxWidth: 230)
+        .frame(maxWidth: 250)
 		.background(
 			RoundedRectangle(cornerRadius: 12)
-				.foregroundStyle(.clear)
+                .foregroundStyle(.kmBeige)
+                .shadow(color: .kmBlack.opacity(0.2), radius: 7, x: 12, y: 5)
 		)
     }
 }
 
 #Preview {
-	RoomCell(
-		room: RoomUI(
-			id: "12b489",
-			name: "Mobile Tech Squad",
-			administrator: UserUI(id: UUID(), name: "Lead")
-		)
-	)
+    ScrollView(.horizontal, showsIndicators: false) {
+        HStack {
+            RoomCell(
+                room: RoomUI(
+                    id: "12b489",
+                    name: "Mobile Tech and Product and Design Squads",
+                    administrator: UserUI(id: UUID(), name: "KawabungaDelToro")
+                )
+            )
+            .padding(.trailing)
+//            RoomCell(
+//                room: RoomUI(
+//                    id: "12b489",
+//                    name: "Mobile Tech Squad",
+//                    administrator: UserUI(id: UUID(), name: "KawabungaElMatador")
+//                )
+//            )
+//            .padding(.trailing)
+//            RoomCell(
+//                room: RoomUI(
+//                    id: "12b489",
+//                    name: "Mobile Tech Squad",
+//                    administrator: UserUI(id: UUID(), name: "Lead")
+//                )
+//            )
+//            .padding(.trailing)
+        }
+        .padding(.vertical)
+    }
 }
--- Kooma/RoomsList/Views/RoomsListView.swift
+++ Kooma/RoomsList/Views/RoomsListView.swift
@@ -6,23 +6,31 @@ struct RoomsListView: View {
 	@Environment(UserManager.self) private var userManager
 	@State private var showYourNextRoom = false
     var body: some View {
-		ZStack(alignment: .bottom) {
-			ScrollView(.vertical, showsIndicators: false) {
-				ForEach(self.roomsListVM.rooms) { room in
-					RoomCell(room: room)
-						.padding(.horizontal, 12)
-						.onTapGesture {
-							print("Room' id: \(room.id)")
-						}
-				}
-				Spacer()
-			}
-			MainButton(text: "New Room", maxWidth: 142) {
-				self.showYourNextRoom = true
-			}
-			.frame(maxWidth: .infinity, alignment: .trailing)
-			.padding(.trailing, 38)
-		}
+            HStack {
+                TextHeading600(text: "Your Rooms")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 12)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    ForEach(self.roomsListVM.rooms) { room in
+                        RoomCell(room: room)
+                            .padding(.horizontal, 12)
+                            .onTapGesture {
+                                print("Room' id: \(room.id)")
+                            }
+                    }
+                }
+                TextHeading600(text: "Rooms you are invited")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 12)
+                
+                Spacer()
+
+                MainButton(text: "New Room", maxWidth: 142) {
+                    self.showYourNextRoom = true
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .padding(.trailing, 38)
+            }
 		.onAppear {
 			print("Rooms' count in RoomListView: \(roomsListVM.rooms.count)")
 		}
@@ -44,4 +52,6 @@ struct RoomsListView: View {
 
 #Preview {
     RoomsListView()
+        .environment(RoomsListViewModel())
+        .environment(UserManager())
 }
